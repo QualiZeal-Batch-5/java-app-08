130a259b6e4abb52d2af2a7a85037fc4b6800862
KBhushanakumari <bhushana1805@gmail.com>
2024-10-28 23:21:45 +0530

    Performed an operation to generate the FACTORIAL of a particular number.

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,13 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp08.operation.FactorialNumber;
+
 public class App {
 	public static void main(String[] args) {
+		FactorialNumber factorialNumber = new FactorialNumber();
+		System.out.println(factorialNumber.generateFactorialNumber(1));
+		System.out.println(factorialNumber.generateFactorialNumber(-1));
+		System.out.println(factorialNumber.generateFactorialNumber(0));
 		
 	}
 }
