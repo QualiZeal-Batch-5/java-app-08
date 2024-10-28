fc91e82f457a1ff5edb7b2a9b54854c670f15c20
Bandaru Udaysri <bandaruudaysri2002@gmail.com>
2024-10-28 17:58:28 +0530

    Added

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,12 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp08.operation.FactorialNumber;
+
 public class App {
 	public static void main(String[] args) {
-		
+		FactorialNumber factorialNumber = new FactorialNumber();
+		System.out.println(factorialNumber.defineFactorialNumber());
+		System.out.println(factorialNumber.generateFactorialNumber(5));
+
 	}
 }
