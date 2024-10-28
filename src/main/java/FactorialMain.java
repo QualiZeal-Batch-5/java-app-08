adfe09269bf506c01ba010a0c5713af371e8641a
Sri Harika <nookalasriharika@gmail.com>
2024-10-28 18:50:17 +0530

    FactorialNumber

---
+++ src/main/java/FactorialMain.java
@@ -0,0 +1,9 @@
+package com.qualizeal.javaapp08.operation;
+
+public class FactorialMain {
+	public static void main(String args[]) {
+		FactorialNumber factorialNumber = new FactorialNumber();
+		System.out.println(factorialNumber.generateFactorialNumber(5));
+	}
+
+}
