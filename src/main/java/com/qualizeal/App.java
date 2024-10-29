42646772d4c07e8b5eb580d338f25b0728f6abdf
Poloju Lahari <poloju.lahari@qualizeal.com>
2024-10-29 14:37:26 +0530

    Factorial Number

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,12 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp08.operation.FactorialNumber;
+
 public class App {
 	public static void main(String[] args) {
-		
-	}
+		FactorialNumber factorialNumber = new FactorialNumber();
+		int res = factorialNumber.generateFactorialNumber(4);
+		System.out.println(res);
+	 }
 }
+ 
